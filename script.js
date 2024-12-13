a88a337383896128534396975e46b1192fc3fd5d
parama848 <parama8148497159@gmail.com>
2024-12-13 09:45:48 +0530

    feat : index file added

---
+++ script.js
@@ -0,0 +1,15 @@
+var typed = new Typed ('.element', {
+    strings: ['Learning Frontend Developer... ', 'Learning Backend Developer... ','FullStack Developer!'],
+    typeSpeed: 120,
+    backSpeed:20,
+    bakDelay:1600,
+    loop:true
+  });
+
+  var typed = new Typed ('#name', {
+    strings: ['Hello Everyone!','I am','Paranthaman Renugopal'],
+    typeSpeed: 120,
+    backSpeed:20,
+    bakDelay:1600,
+    loop:false
+  });
